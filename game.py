632ff2bca2cf9ecd90b1a9ef29879b72b2fdb785
Linus002 <linuswambugu2@gmail.com>
2023-02-16 11:21:48 +0300

    added guessing_game functions

--- game.py
+++ game.py
@@ -1,16 +1,28 @@
 # this programme demonstrates a guessing game
 # gets user's input
-user_name = input("What's your name?")
-print("Hello there" + user_name + "!")
+player = input("What's your name?")
+print("Hello there" + player + "!")
 
-counter = 0
 # generates a random number
 from random import randint
+number = randint(1, 100)
+
+counter = 0
 
 
 # using a while loop
 while counter <5:
-    user_name = eval(input("Pick a number of your choice"))
+    user_number = eval(input("Enter a number:"))
+    counter += 1
+
+# checks if usernumber is equal to randomly generated number
+    if user_number < number:
+        print("Your guess is too low.") 
+    elif user_number > number:
+        print("Your guess is too high")
+    elif user_number == number:
+        print("Correct.You win")
+        break
 
 
-# checks if user input is equal to random number
+    
